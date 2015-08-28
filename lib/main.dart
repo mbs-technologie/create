aa2c6f4ac738bc043489b424573ac903d814585c
Misha Dynin <dynin@google.com>
2015-08-28 15:11:59 -0700

    Display 'Hello, world!' as a Sky widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
-main() {
-  print("Hello, world!");
+// Copyright 2015 The Chromium Authors. All rights reserved.
+
+import 'package:sky/widgets.dart';
+
+class HelloWorldApp extends App {
+  Widget build() {
+    return new Center(child: new Text('Hello, world!',
+        style : new TextStyle(fontSize: 68.0)));
+  }
+}
+
+void main() {
+  runApp(new HelloWorldApp());
 }
