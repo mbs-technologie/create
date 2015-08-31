b3c93d998827990ba3e56a9b3c70ec1e15814c62
Misha Dynin <dynin@google.com>
2015-08-31 15:09:21 -0700

    Added title and theme to the app. Renamed the app to CreateApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 
 import 'package:sky/widgets.dart';
+import 'package:sky/theme/colors.dart' as colors;
 
 enum Dimensions { Modules, Schema, Paramaters, Library, Services, Views, Styles, Data, Launch }
 enum Modules { Core, Meta, Demo }
 
-class HelloWorldApp extends App {
+class CreateApp extends App {
+  static const String APP_TITLE = "Create!";
+
   Widget build() {
-    return new Center(child: new Text('Hello, world!',
+    ThemeData theme = new ThemeData(
+      brightness: ThemeBrightness.light,
+      primarySwatch: colors.Teal
+    );
+
+    final Widget content = new Center(child: new Text('Hello, world!',
         style : new TextStyle(fontSize: 68.0)));
+
+    return new Theme(
+      data: theme,
+      child: new Title(
+        title: APP_TITLE,
+        child: content
+      )
+    );
   }
 }
 
 void main() {
-  runApp(new HelloWorldApp());
+  runApp(new CreateApp());
 }
