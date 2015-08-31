6de0cc7be009609626d842bb897158f05f9c3aee
Misha Dynin <dynin@google.com>
2015-08-31 15:31:18 -0700

    Added scaffold and toolbar to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,53 @@ enum Modules { Core, Meta, Demo }
 class CreateApp extends App {
   static const String APP_TITLE = "Create!";
 
+  Widget buildBody() {
+    return new Center(child: new Text('Hello, world!',
+        style : new TextStyle(fontSize: 68.0)));
+  }
+
+  Widget buildToolBar() {
+    return new ToolBar(
+        left: new IconButton(
+          icon: "navigation/menu",
+          onPressed: _handleOpenDrawer),
+        center: new Text(APP_TITLE),
+        right: [
+          new IconButton(
+            icon: "action/search",
+            onPressed: _handleBeginSearch),
+          new IconButton(
+            icon: "navigation/more_vert",
+            onPressed: _handleShowMenu)
+        ]
+      );
+  }
+
+  void _handleOpenDrawer() => null; // TODO
+  void _handleBeginSearch() => null; // TODO
+  void _handleShowMenu() => null; // TODO
+
+  Widget buildScaffold() {
+    return new Scaffold(
+      toolbar: buildToolBar(),
+      body: buildBody(),
+      snackBar: null,
+      floatingActionButton: null,
+      drawer: null
+    );
+  }
+
   Widget build() {
     ThemeData theme = new ThemeData(
       brightness: ThemeBrightness.light,
       primarySwatch: colors.Teal
     );
 
-    final Widget content = new Center(child: new Text('Hello, world!',
-        style : new TextStyle(fontSize: 68.0)));
-
     return new Theme(
       data: theme,
       child: new Title(
         title: APP_TITLE,
-        child: content
+        child: buildScaffold()
       )
     );
   }
