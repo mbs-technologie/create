dc6a0e0d3c4dd752b897242b09c01db8a040576d
Misha Dynin <dynin@google.com>
2015-12-16 16:24:50 -0800

    Working on meta, added Output and EnumValueConstruct.

--- lib/meta.dart
+++ lib/meta.dart
@@ -2,29 +2,32 @@
 
 library meta;
 
+import 'dart:io';
+
 //import 'elements.dart';
 
 /// Identifier consisting of one or more segments.
 class Identifier {
   List<String> _segments;
 
-  Identifier(this._segments) {
-    assert (_segments.isNotEmpty);
+  Identifier(List<String> segments) {
+    assert(segments.isNotEmpty);
+    _segments = new List.unmodifiable(segments.map((s) => s.toLowerCase()));
   }
 
-  String underscoreLower() {
+  String get underscoreLower {
     return _segments.join('_');
   }
 
-  String underscoreUpper() {
-    return _segments.map((s) => (s.toUpperCase())).join('_');
+  String get underscoreUpper {
+    return _segments.map((s) => s.toUpperCase()).join('_');
   }
 
-  String camelCaseLower() {
+  String get camelCaseLower {
     return _segments[0] + _segments.skip(1).map(_upperCaseFirst).join();
   }
 
-  String camelCaseUpper() {
+  String get camelCaseUpper {
     return _segments.map(_upperCaseFirst).join();
   }
 
@@ -32,7 +35,6 @@ class Identifier {
     List<String> newSegments = new List.from(_segments);
     newSegments.addAll(other._segments);
     return new Identifier(newSegments);
-
   }
 
   static String _upperCaseFirst(String s) {
@@ -40,12 +42,36 @@ class Identifier {
   }
 }
 
-void main() {
-  var test = new Identifier(['foo', 'bar', 'baz']);
-  print('ul: ' + test.underscoreLower());
-  print('uu: ' + test.underscoreUpper());
-  print('cl: ' + test.camelCaseLower());
-  print('cu: ' + test.camelCaseUpper());
+class Output {
+  writeLine(String s) {
+    stdout.write('$s\n');
+  }
+}
 
-  print('joined: ' + test.append(test).camelCaseLower());
+class EnumValueConstruct {
+  Identifier name;
+
+  EnumValueConstruct(this.name);
+
+  void write(Output output) {
+    var enumType = new Identifier(['Themed', 'Style']);
+    output.writeLine(
+        'const ${enumType.camelCaseUpper} ${name.underscoreUpper} = ' +
+            'const ${enumType.camelCaseUpper}("Title");');
+  }
+}
+
+void test_identifier() {
+  var test = new Identifier(['foo', 'Bar', 'baz']);
+  print('ul: ' + test.underscoreLower);
+  print('uu: ' + test.underscoreUpper);
+  print('cl: ' + test.camelCaseLower);
+  print('cu: ' + test.camelCaseUpper);
+
+  print('joined: ' + test.append(test).camelCaseLower);
+}
+
+void main() {
+  var output = new Output();
+  new EnumValueConstruct(new Identifier(['Title', 'style'])).write(output);
 }
